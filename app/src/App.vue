c8e36aa96ab3a07780e9ee3f6aae44347d08e075
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2025-05-12 16:37:22 +0800

    feat: implement general ui styles throughout site

--- app/src/App.vue
+++ app/src/App.vue
@@ -3,7 +3,9 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="max-w-3xl mx-auto p-4">
+  <div class="w-screen h-screen bg-slate-50">
+    <div class="max-w-3xl mx-auto p-4 text-slate-950 font-[Bricolage_Grotesque]">
     <RouterView />
+  </div>
   </div>
 </template>
