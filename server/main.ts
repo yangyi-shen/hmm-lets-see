2c883334d9b098ea209587a0579bc5a272890ebe
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2025-05-12 15:28:25 +0800

    style: reformat main.ts

--- server/main.ts
+++ server/main.ts
@@ -1,15 +1,20 @@
-import { fetchCrossrefWorks, fetchCrossrefWorksAnalysis, fetchSearchKeywords } from "./services/service.js";
+import {
+	fetchCrossrefWorks,
+	fetchCrossrefWorksAnalysis,
+	fetchSearchKeywords,
+} from "./services/service.js";
 
-const question = "How, in quantitative terms, does drinking alchohol impair the mental development of a teenager?";
+const question =
+	"How, in quantitative terms, does drinking alchohol impair the mental development of a teenager?";
 
 const keywords = await fetchSearchKeywords(question);
 console.log("KEYWORDS: ", keywords);
 
-const works = await fetchCrossrefWorks(
-	keywords,
-	1 // increase count once I have Groq developer account for higher rate limit
+const works = await fetchCrossrefWorks(keywords, 5);
+console.log(
+	"EST. TOKENS: ",
+	works.reduce((acc: number, work: string) => work.length + acc, 0) / 4
 );
-console.log("EST. TOKENS: ", works.reduce((acc: number, work: string) => work.length + acc, 0) / 4);
 
 const analysis = await fetchCrossrefWorksAnalysis(question, works);
 console.log("ANALYSIS: ", analysis);
