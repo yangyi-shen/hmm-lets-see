41cfa09f296cde622eafb7777f320437b4dd114c
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2025-05-12 16:41:06 +0800

    feat: add text for main page & rename component

---
+++ app/src/views/MainView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <section>
+      <p class="text-2xl font-bold mb-1">Hmm, let's see... 🤔</p>
+      <p>Type a question in the searchbox below, and the relevant research papers will be fetched from Crossref, and data-crunched by AI to produce a reasonably educated answer.</p>
+    </section>
+    <footer>
+      <p>PS: This is my first project involving AI and technologies like LangChain, so the results will probably be less than helpful! However, I hope you nonetheless find it a little interesting.</p>
+    </footer>
+  </main>
+</template>
