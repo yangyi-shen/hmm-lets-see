903db5032858dcee619c62c1e4fcddd73eca3dc7
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2025-05-13 14:56:36 +0800

    fix: class name typo

--- app/src/App.vue
+++ app/src/App.vue
@@ -3,7 +3,7 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="w-screen h-min-screen bg-slate-50">
+  <div class="w-screen min-h-screen bg-slate-50">
     <div class="max-w-3xl mx-auto p-4 text-slate-950 font-[Bricolage_Grotesque]">
     <RouterView />
   </div>
